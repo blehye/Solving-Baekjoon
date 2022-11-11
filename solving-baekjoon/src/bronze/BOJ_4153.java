21cb2d68d66577bd9dddc4ec794aeeb0db081100
blehye <hhw0850@naver.com>
2022-11-11 23:13:23 +0900

    4153 - 직각삼각형

--- solving-baekjoon/src/bronze/BOJ_4153.java
+++ solving-baekjoon/src/bronze/BOJ_4153.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class 직각삼각형_4153 {
+public class BOJ_4153 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
