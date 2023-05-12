32515675790b56eaa174be2bcb82ad8d4aa7e7ee
blehye <hhw0850@naver.com>
2023-05-13 00:10:21 +0900

    2738 - 행렬 덧셈

---
+++ solving-baekjoon/src/bronze/BOJ_2738.java
@@ -0,0 +1,50 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2738 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int[][] arr1 = new int[n][m];
+		int[][] arr2 = new int[n][m];
+		int[][] result = new int[n][m];
+		
+		for(int i=0; i<n; i++) {
+			StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
+			for(int j=0; j<m; j++) {
+				arr1[i][j] = Integer.parseInt(st1.nextToken());
+			}
+		}
+		
+		for(int i=0; i<n; i++) {
+			StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
+			for(int j=0; j<m; j++) {
+				arr2[i][j] = Integer.parseInt(st2.nextToken());
+			}
+		}
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				result[i][j] = arr1[i][j] + arr2[i][j];
+			}
+		}
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				System.out.print(result[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
