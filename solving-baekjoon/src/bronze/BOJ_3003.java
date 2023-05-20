38bacec4d08ed2e6135e6563c7de433dc6ff2501
blehye <hhw0850@naver.com>
2023-05-20 18:55:08 +0900

    3003 - 킹, 퀸, 룩, 비숍, 나이트, 폰

---
+++ solving-baekjoon/src/bronze/BOJ_3003.java
@@ -0,0 +1,26 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_3003 {
+	
+	public static void main(String[] args) throws IOException {
+		// 킹1 퀸1 룩2 비숍2 나이트2 폰8
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int king = Integer.parseInt(st.nextToken());
+		int queen = Integer.parseInt(st.nextToken());
+		int rook = Integer.parseInt(st.nextToken());
+		int bishop = Integer.parseInt(st.nextToken());
+		int knight = Integer.parseInt(st.nextToken());
+		int pawn = Integer.parseInt(st.nextToken());
+		
+		System.out.println((1-king) + " " + (1-queen) + " " + (2-rook) + " " + (2-bishop) + " " + (2-knight) + " " + (8-pawn));
+	}
+
+}
