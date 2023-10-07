72eafb74fa48c12ef2a85c87fc2301d8999730a4
blehye <hhw0850@naver.com>
2023-10-07 19:04:07 +0900

    15664 - N과 M (10)

---
+++ solving-baekjoon/src/silver/BOJ_15664.java
@@ -0,0 +1,56 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+
+public class BOJ_15664 {
+
+    public static StringBuilder sb = new StringBuilder();
+    public static boolean[] visited;
+    public static int[] arr;
+    public static int[] numList;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        String str = br.readLine();
+        numList = Stream.of(str.split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(numList);
+
+        visited = new boolean[n];
+        arr = new int[m];
+
+        dfs(n, m, 0, 0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int n, int m, int depth, int start) {
+        if (depth == m) {
+            for (int num : arr) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int before = 0;
+        for (int i = start; i < n; i++) {
+            if (!visited[i] && before != numList[i]) {
+                visited[i] = true;
+                arr[depth] = numList[i];
+                before = numList[i];
+                dfs(n, m, depth + 1, i + 1);
+                visited[i] = false;
+            }
+        }
+
+    }
+
+}
